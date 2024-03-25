1d3ae8c1f53c373dd950b893a20217557f9273c3
Davi Alpiano <davialpiano@gmail.com>
2024-03-25 17:04:11 -0300

    Req 1

---
+++ src/components/Table.tsx
@@ -0,0 +1,51 @@
+import { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+function Table() {
+  const context = useContext(PlanetsContext);
+  const { planets } = context;
+
+  return (
+    <div>
+      <table border={ 1 }>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planets.map((planet) => (
+            <tr key={ planet.name }>
+              <th>{planet.name}</th>
+              <th>{planet.rotation_period}</th>
+              <th>{planet.orbital_period}</th>
+              <th>{planet.diameter}</th>
+              <th>{planet.climate}</th>
+              <th>{planet.gravity}</th>
+              <th>{planet.terrain}</th>
+              <th>{planet.surface_water}</th>
+              <th>{planet.population}</th>
+              <th>{planet.films}</th>
+              <th>{planet.created}</th>
+              <th>{planet.edited}</th>
+              <th>{planet.url}</th>
+            </tr>))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
---
+++ src/context/PlanetsProvider.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import PlanetsContext from './PlanetsContext';
+import { Planet } from '../types';
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+};
+
+function PlanetsProvider({ children }: ThemeProviderProps) {
+  const [planets, setPlanets] = useState<Planet[]>([]);
+
+  useEffect(() => {
+    const FetchApi = async () => {
+      try {
+        const dados = await fetch('https://swapi.dev/api/planets');
+        const resultado = await dados.json();
+        const noResidents = resultado.results.map((planet: Planet) => {
+          delete planet.residents;
+          return planet;
+        });
+        setPlanets(noResidents);
+      } catch (erro) {
+        console.error(erro);
+      }
+    };
+
+    FetchApi();
+  }, []);
+
+  return (
+    <PlanetsContext.Provider value={ { planets } }>
+      { children }
+    </PlanetsContext.Provider>
+  );
+}
+
+export default PlanetsProvider;
